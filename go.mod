f4f8706bde673c65fbe0a4e1051d6afced2b3c1d
andreee94 <andrea26.4.94@gmail.com>
2021-06-06 20:03:47 +0200

    included freenom api, but broken imports

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	github.com/jetstack/cert-manager v1.3.1
+	github.com/tzwsoho/go-freenom v0.0.0-20201109024018-fe2c93cab446
 	k8s.io/apiextensions-apiserver v0.21.1
 	k8s.io/client-go v0.21.1
+	k8s.io/klog v1.0.0
 )
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
 
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	// v1 "k8s.io/apiextensions-apiserver/pkg/apis/core/v1"
+
 	//"k8s.io/client-go/kubernetes"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog"
 
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
+	cmmeta "github.com/jetstack/cert-manager/pkg/apis/meta/v1"
+	"github.com/jetstack/cert-manager/pkg/issuer/acme/dns/util"
+
+	"github.com/tzwsoho/go-freenom/freenom"
 )
 
 var GroupName = os.Getenv("GROUP_NAME")
@@ -26,25 +36,25 @@ func main() {
 	// webhook, where the Name() method will be used to disambiguate between
 	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
-		&customDNSProviderSolver{},
+		&freenomDNSProviderSolver{},
 	)
 }
 
-// customDNSProviderSolver implements the provider-specific logic needed to
+// freenomDNSProviderSolver implements the provider-specific logic needed to
 // 'present' an ACME challenge TXT record for your own DNS provider.
 // To do so, it must implement the `github.com/jetstack/cert-manager/pkg/acme/webhook.Solver`
 // interface.
-type customDNSProviderSolver struct {
+type freenomDNSProviderSolver struct {
 	// If a Kubernetes 'clientset' is needed, you must:
 	// 1. uncomment the additional `client` field in this structure below
 	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
 	// 3. uncomment the relevant code in the Initialize method below
 	// 4. ensure your webhook's service account has the required RBAC role
 	//    assigned to it for interacting with the Kubernetes APIs you need.
-	//client kubernetes.Clientset
+	client kubernetes.Clientset
 }
 
-// customDNSProviderConfig is a structure that is used to decode into when
+// freenomDNSProviderConfig is a structure that is used to decode into when
 // solving a DNS01 challenge.
 // This information is provided by cert-manager, and may be a reference to
 // additional configuration that's needed to solve the challenge for this
@@ -58,14 +68,14 @@ type customDNSProviderSolver struct {
 // You should not include sensitive information here. If credentials need to
 // be used by your provider here, you should reference a Kubernetes Secret
 // resource and fetch these credentials using a Kubernetes clientset.
-type customDNSProviderConfig struct {
+type freenomDNSProviderConfig struct {
 	// Change the two fields below according to the format of the configuration
 	// to be decoded.
 	// These fields will be set by users in the
 	// `issuer.spec.acme.dns01.providers.webhook.config` field.
 
-	//Email           string `json:"email"`
-	//APIKeySecretRef v1alpha1.SecretKeySelector `json:"apiKeySecretRef"`
+	Email              string                   `json:"email"`
+	PassswordSecretRef cmmeta.SecretKeySelector `json:"passwordSecretRef"`
 }
 
 // Name is used as the name for this DNS solver when referencing it on the ACME
@@ -74,7 +84,7 @@ type customDNSProviderConfig struct {
 // solvers configured with the same Name() **so long as they do not co-exist
 // within a single webhook deployment**.
 // For example, `cloudflare` may be used as the name of a solver.
-func (c *customDNSProviderSolver) Name() string {
+func (c *freenomDNSProviderSolver) Name() string {
 	return "freenom"
 }
 
@@ -83,16 +93,41 @@ func (c *customDNSProviderSolver) Name() string {
 // This method should tolerate being called multiple times with the same value.
 // cert-manager itself will later perform a self check to ensure that the
 // solver has correctly configured the DNS provider.
-func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+func (c *freenomDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 	cfg, err := loadConfig(ch.Config)
 	if err != nil {
 		return err
 	}
 
-	// TODO: do something more useful with the decoded configuration
 	fmt.Printf("Decoded configuration %v", cfg)
 
-	// TODO: add code that sets a record in the DNS provider's console
+	password, err := c.getSecretKey(cfg.PassswordSecretRef, ch.ResourceNamespace)
+	if nil != err {
+		return err
+	}
+
+	err = freenom.Login(cfg.Email, password)
+	if nil != err {
+		return err
+	}
+
+	zone := util.UnFqdn(ch.ResolvedZone)
+	fqdn := util.UnFqdn(ch.ResolvedFQDN)
+	subName := fqdn[:len(fqdn)-len(zone)-1]
+
+	err = freenom.AddRecord(ch.ResolvedZone, []freenom.DomainRecord{
+		{
+			Type:     freenom.RecordTypeTXT,
+			Name:     subName,
+			TTL:      3600,
+			Value:    ch.Key, // Token to present as TXT
+			Priority: 100,
+		},
+	})
+	if nil != err {
+		return err
+	}
+
 	return nil
 }
 
@@ -102,8 +137,40 @@ func (c *customDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
 // value provided on the ChallengeRequest should be cleaned up.
 // This is in order to facilitate multiple DNS validations for the same domain
 // concurrently.
-func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+func (c *freenomDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
 	// TODO: add code that deletes a record from the DNS provider's console
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Decoded configuration %v", cfg)
+
+	password, err := c.getSecretKey(cfg.PassswordSecretRef, ch.ResourceNamespace)
+	if nil != err {
+		return err
+	}
+
+	err = freenom.Login(cfg.Email, password)
+	if nil != err {
+		return err
+	}
+
+	zone := util.UnFqdn(ch.ResolvedZone)
+	fqdn := util.UnFqdn(ch.ResolvedFQDN)
+	subName := fqdn[:len(fqdn)-len(zone)-1]
+
+	err = freenom.DeleteRecord(ch.ResolvedZone, &freenom.DomainRecord{
+		Type:     freenom.RecordTypeTXT,
+		Name:     subName,
+		TTL:      3600,
+		Value:    ch.Key, // Token to present as TXT
+		Priority: 100,
+	})
+	if nil != err {
+		return err
+	}
+
 	return nil
 }
 
@@ -116,16 +183,16 @@ func (c *customDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
 // provider accounts.
 // The stopCh can be used to handle early termination of the webhook, in cases
 // where a SIGTERM or similar signal is sent to the webhook process.
-func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+func (c *freenomDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
 	///// UNCOMMENT THE BELOW CODE TO MAKE A KUBERNETES CLIENTSET AVAILABLE TO
 	///// YOUR CUSTOM DNS PROVIDER
 
-	//cl, err := kubernetes.NewForConfig(kubeClientConfig)
-	//if err != nil {
-	//	return err
-	//}
-	//
-	//c.client = cl
+	cl, err := kubernetes.NewForConfig(kubeClientConfig)
+	if err != nil {
+		return err
+	}
+
+	c.client = *cl
 
 	///// END OF CODE TO MAKE KUBERNETES CLIENTSET AVAILABLE
 	return nil
@@ -133,8 +200,8 @@ func (c *customDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stop
 
 // loadConfig is a small helper function that decodes JSON configuration into
 // the typed config struct.
-func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
-	cfg := customDNSProviderConfig{}
+func loadConfig(cfgJSON *extapi.JSON) (freenomDNSProviderConfig, error) {
+	cfg := freenomDNSProviderConfig{}
 	// handle the 'base case' where no configuration has been provided
 	if cfgJSON == nil {
 		return cfg, nil
@@ -144,4 +211,21 @@ func loadConfig(cfgJSON *extapi.JSON) (customDNSProviderConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+// getSecretKey fetch a secret key based on a selector and a namespace
+func (c *freenomDNSProviderSolver) getSecretKey(secret cmmeta.SecretKeySelector, namespace string) (string, error) {
+	klog.V(6).Infof("retrieving key `%s` in secret `%s/%s`", secret.Key, namespace, secret.Name)
+
+	sec, err := c.client.CoreV1().Secrets(namespace).Get(context.Background(), secret.Name, metav1.GetOptions{})
+	if err != nil {
+		return "", fmt.Errorf("secret `%s/%s` not found", namespace, secret.Name)
+	}
+
+	data, ok := sec.Data[secret.Key]
+	if !ok {
+		return "", fmt.Errorf("key `%q` not found in secret `%s/%s`", secret.Key, namespace, secret.Name)
+	}
+
+	return string(data), nil
 }
